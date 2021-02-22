3b130a752bb77ccfa14bbeeec00f6f3188efa405
danielstepanek <56407814+danielstepanek@users.noreply.github.com>
2021-02-22 10:10:11 -0700

    First commit

---
+++ Assets/LegSolver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegSolver : MonoBehaviour
+{
+    [SerializeField] GameObject[] legs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach(GameObject leg in legs)
+        {
+            Gizmos.DrawSphere(leg.transform.position, .1f);
+        }
+    }
+}
