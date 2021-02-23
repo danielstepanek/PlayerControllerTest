5c1a52058ab9cb90a00c4261637618f034d7201d
danielstepanek <56407814+danielstepanek@users.noreply.github.com>
2021-02-22 22:03:58 -0700

    Added Leg.cs as a data class

---
+++ Assets/Leg.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+
+public class Leg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    CCDIK ccdik;
+
+    private void Awake()
+    {
+        ccdik = GetComponent<CCDIK>();
+
+        IKSolverHeuristic.Bone[] bones = ccdik.solver.bones;
+        print(bones.Length);
+        foreach(IKSolverHeuristic.Bone bone in bones)
+        {
+            print(bone.transform.position);
+        }
+
+
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
