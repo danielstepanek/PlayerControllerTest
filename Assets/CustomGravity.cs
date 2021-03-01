e05f439cfeef6b7699dcbca1e044ff38e2c519ae
danielstepanek <56407814+danielstepanek@users.noreply.github.com>
2021-02-28 20:13:12 -0700

    Custom Gravity CLC

---
+++ Assets/CustomGravity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CustomGravity
+{
+	public static Vector3 GetGravity(Vector3 position)
+	{
+		return position.normalized * Physics.gravity.y;
+	}
+	public static Vector3 GetUpAxis(Vector3 position)
+	{
+		return position.normalized;
+	}
+	public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis)
+	{
+		upAxis = position.normalized;
+		return upAxis * Physics.gravity.y;
+	}
+}
---
+++ Assets/CustomGravityRigidBody.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CustomGravityRigidBody : MonoBehaviour
+{
+
+	Rigidbody body;
+	float floatDelay;
+
+	[SerializeField]
+	bool floatToSleep = false;
+
+	void Awake()
+	{
+		body = GetComponent<Rigidbody>();
+		body.useGravity = false;
+	}
+
+	void FixedUpdate()
+	{
+		if (body.IsSleeping())
+		{
+			floatDelay = 0f;
+			return;
+		}
+
+		if (body.velocity.sqrMagnitude < 0.0001f)
+		{
+			if (floatToSleep)
+			{
+				floatDelay += Time.deltaTime;
+				if (floatDelay >= 1f)
+				{
+					return;
+				}
+				else
+				{
+					floatDelay = 0f;
+				}
+			}
+		}
+		body.AddForce(
+			CustomGravity.GetGravity(body.position), ForceMode.Acceleration);
+	}
+}
